cb18db430c8802f083248ccd8d60dd181bcd7b53
KrusH <krushik@ukr.net>
2016-06-02 19:30:07 +0300

    ContentProviders

--- app/src/main/java/info/krushik/android/myapplication101/MainActivity.java
+++ app/src/main/java/info/krushik/android/myapplication101/MainActivity.java
@@ -1,12 +1,17 @@
 package info.krushik.android.myapplication101;
 
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,6 +20,7 @@ public class MainActivity extends AppCompatActivity
     //реализация интерфейса LoaderCallbacks(можно писать в любом классе и обратится в этом классе)
 
     private ListView mListView;
+    private SaveTask mSaveTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,20 @@ public class MainActivity extends AppCompatActivity
 
         mListView = (ListView) findViewById(R.id.listView); //находим ListView
         getSupportLoaderManager().initLoader(0, null, this); //SupportLoaderManager()-поддержка старых версий v4
+    }
+
+    public void OnClick(View v){
+        mSaveTask = new SaveTask();
+        mSaveTask.execute(new Student("Ivan", "Ivanov", 22));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(mSaveTask !=null){
+            mSaveTask.cancel(true);
+        }
     }
 
     //Создание Loader
@@ -49,7 +69,50 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    class SaveTask extends AsyncTask<Student, Void, Long>{
+    //AsyncTask для сохранения студента(однозадачные)
+    class SaveTask extends AsyncTask<Student, Void, Long>{//(принимает студентов, ,возвращает long(id"шку))
+        private ProgressDialog mDialog;
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            mDialog = new ProgressDialog(MainActivity.this);//говорим пользователю что чтото делается
+            mDialog.setMessage("Saving...");
+            mDialog.setCancelable(false);
+            mDialog.show();
+        }
+
+        @Override
+        protected Long doInBackground(Student... params) {//doInBackground добавляет по умолчанию
+            long id = 0;
+            Student student = params[0];
+
+            try {
+                Uri uri = Uri.parse("content://info.krushik.android.myapplication10/students");//объявляем новый Uri, убеждаемся что он правильный
+
+                ContentValues values = new ContentValues();
+
+                values.put("FirstName", student.FirstName);
+                values.put("LastName", student.LastName);
+                values.put("Age", student.Age);
+
+                getContentResolver().insert(uri, values);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return id;
+        }
+
+        //передаем на интерфейс ответ что мы сохранили студента и его id"шка вот такая
+        @Override
+        protected void onPostExecute(Long aLong) {
+            if (mDialog != null) {//если mDialog существует
+                mDialog.dismiss();//мы его закрываем
+            }
+
+            getSupportLoaderManager().restartLoader(0, null, MainActivity.this);//перечитуем заново наших студентов
+        }
     }
 }
